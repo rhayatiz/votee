b1e4f310b86bd9529a4532f49200eda80100b93a
rhayatiz <rhayatizakaria@gmail.com>
2024-01-17 00:48:54 +0100

    build 1

---
+++ public/build/assets/QuestionTitle-3435c31c.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-847477af.js";import{T as i}from"./Footer-57e48d44.js";function n({title:t,index:r}){const o=r?r+". ":"";return s.jsxs(i,{mb:"lg",order:4,children:[o,t]})}export{n as Q};
